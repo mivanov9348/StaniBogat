3d5d97f068cf44cee4632bc5de1ea160c2e07a78
mivanov93 <72788178+mivanov9348@users.noreply.github.com>
2022-01-23 12:32:37 +0200

    Add files via upload

---
+++ stanibogat.sql
@@ -0,0 +1,67 @@
+
+Insert into Scales(Number,[Money])values
+(1,500),
+(2,1000),
+(3,2000),
+(4,3000),
+(5,5000),
+(6,7500),
+(7,10000),
+(8,15000),
+(9,25000),
+(10,50000),
+(11,75000),
+(12,150000),
+(13,250000),
+(14,500000),
+(15,1000000)
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Колко цвята е знамето на България?','3','1','2','4','A',1),
+(N'Какво се прави и поправя от обущар?',N'Обувки',N'Пътища',N'Прозорци',N'Бижута','A',1),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Кога или какво улавя Интерпол?',N'Депутати',N'Животни',N'Настинка',N'Престъпници','D',2),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'С какъв спорт е известен Лионел Меси?',N'Футбол',N'Баскетбол',N'Волейбол',N'Хокей','A',3),
+(N'В кой български град се произвежда бира с марката „Алмус“ ?',N'Луковит',N'Лом',N'Ловеч',N'Лясковец','B',3),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Кое животно има 3 сърца, 9 мозъка и синя кръв?',N'Крава',N'Котка',N'Октопод',N'Куче','C',4),
+(N'Какво прави сомнамбулът насън?',N'Върви',N'Подскача',N'Крещи',N'Плаче','A',4),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Кой е написал трагедията "Ромео и Жулиета"?',N'Уилям Шекспир',N'Оскар Уайлд',N'Иван Вазов',N'Стивън Кинг','A',5),
+(N'Какво е фотосинтеза?',N'Разделяне на молекули',N'Метод за обработване на снимки',N'Бухване на бакплувер',N'Преобразуване на светлинна енергия в химическа енергия','D',5)
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Коя е най-голямата пустиня в света?',N'Гоби',N'Сахара',N'Калахари',N'Сирийската','B',6),
+(N'Откъде идва "кюдо", стрелба с лък?',N'Япония',N'Иран',N'Китай',N'Южна Корея','A',6),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Кой е кръстникът на Хари Потър от поредицата "Хари Потър"?',N'Хагрид',N'Сириус Блек',N'Седрик Дигъри',N'Албус Дъмбълдор','B',7),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'В кой град е починал Панайот Хитов?','Русе','Пловдив','Копривщица','Котел','A',8),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Кой антиутопичен роман е за живота в тоталитарно общество, доминирано от „Биг Брадър“?',N'1984',N'Фермата на животните',N'451 градуса по Фаренхайт',N'Да убиеш пресмехулник','A',9),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Къде свети олимпийският огън няколко месеца преди началото на олимпийските игри?',N'Атина',N'Париж',N'Олимпия',N'Вашингтон','C',10),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Козидо е национално ястие в коя държава?',N'Алжир',N'Испания',N'Мароко',N'Португалия','B',11),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Какво означава латинската поговорка "quid pro quo"?',N'Нещо дадено или получено в замяна',N'Аз съм това, което ти ще бъдеш, а ти си това, което аз бях',N'Бързай бавно!',N'Да започнем с най-важното','A',12),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Как се нарича пиратския флаг с череп и кости?',N'Капитан Напин',N'Джон Филипс',N'Уолтър Кенеди',N'Джоли Роджър','D',13),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Кой е единственият сръбски лауреат на Нобелова награда?',N'Иво Андрич',N'Никола Тесла',N'Иван Гутман',N'Миодраг Томич','A',14),
+
+Insert into Quests(Question,A,B,C,D,CorrectAnswer,ScaleId)values
+(N'Кой обект, намиращ се в САЩ, носи най-дългото наименование - Chargoggagoggmanchauggagoggchaubunagungamaugg?',N'Планина',N'Езеро',N'Река',N'Село','B',15)
